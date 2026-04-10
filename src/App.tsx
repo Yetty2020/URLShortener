de27961e0e5f8c3be694497d1c7c86a1b3b4dffe
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-10 01:45:28 +0100

    feat: created AppLayout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 
-
+import {createBrowserRouter} from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout/>,
+      children: []
+    }
+  ])
   return (
     <div>
       
