6014c773cf81f14198816fa283488fe9d77dc7d5
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-11 22:22:28 +0100

    chore: installed dropdown menu from shadcn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ import { Button } from './ui/button'
 function Header() {
     const navigate = useNavigate()
   return (
-    <nav>
+    <nav className='py-4 flex items-center justify-between '>
         <Link to="/" className='text-2xl font-bold text-gray-800'>
         <h3>Shortener</h3>
 
         </Link>
         <div>
-            <Button onClick={() => {navigate("/auth")}}>Login</Button>
+            <Button onClick={() => {navigate("/auth")}} variant='outline'>Login</Button>
         </div>
     </nav>
   )
