e89f075faebaab644fcf97bfacca16c468c65a19
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-11 22:33:51 +0100

    Install Avatart from shadcn ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
 
 function Header() {
     const navigate = useNavigate()
+    const user = false//dummy will be  implemented later on
   return (
     <nav className='py-4 flex items-center justify-between '>
         <Link to="/" className='text-2xl font-bold text-gray-800'>
@@ -11,7 +21,31 @@ function Header() {
 
         </Link>
         <div>
-            <Button onClick={() => {navigate("/auth")}} variant='outline'>Login</Button>
+            {
+                !user ? (
+                    <DropdownMenu>
+  <DropdownMenuTrigger asChild>
+    <Button variant="outline">Open</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuGroup>
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuItem>Profile</DropdownMenuItem>
+      <DropdownMenuItem>Billing</DropdownMenuItem>
+    </DropdownMenuGroup>
+    <DropdownMenuSeparator />
+    <DropdownMenuGroup>
+      <DropdownMenuItem>Team</DropdownMenuItem>
+      <DropdownMenuItem>Subscription</DropdownMenuItem>
+    </DropdownMenuGroup>
+  </DropdownMenuContent>
+</DropdownMenu>
+                ) : (
+                    <Button onClick={() => {navigate("/auth")}} variant='outline'>Login</Button>
+
+                )
+            }
+            
         </div>
     </nav>
   )
