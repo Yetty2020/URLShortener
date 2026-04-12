e70d67ed50a3b17441a8bf577d4668f767e652e3
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-12 20:50:08 +0100

    feat: handle redirect intent on auth page using query params

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { useSearchParams } from "react-router-dom"
 
 export default function auth() {
+  const [searchParams] = useSearchParams()
   return (
+
     <div>
+      <h1>
+        {searchParams.get("CreateNew")? "Hold up, lets login first...": "Login / Signup"}
+      </h1>
       
     </div>
   )
