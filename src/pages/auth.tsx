a119eb40e167fa3bc7a67db4186ef392076a7186
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-17 21:48:38 +0100

    feat: set up authentification for user login

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,9 @@
 import { useSearchParams } from "react-router-dom"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Login from "@/components/Login"
+import Signup from "@/components/Signup"
 
-export default function auth() {
+export default function Auth() {
   const [searchParams] = useSearchParams()
   return (
 
@@ -8,7 +11,14 @@ export default function auth() {
       <h1>
         {searchParams.get("CreateNew")? "Hold up, lets login first...": "Login / Signup"}
       </h1>
-      
+      <Tabs defaultValue="login" className="w-[400px] flex flex-col ">
+  <TabsList>
+    <TabsTrigger value="login">Login</TabsTrigger>
+    <TabsTrigger value="signup">SignUp</TabsTrigger>
+  </TabsList>
+  <TabsContent value="login"><Login/> </TabsContent>
+  <TabsContent value="signup"><Signup/></TabsContent>
+</Tabs>
     </div>
   )
 }
