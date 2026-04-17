fe6fdf06cb119467874740bf314aa111fe17bebd
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-17 22:02:41 +0100

    chore: Install react spinner for better ux

--- src/db/apiAuth.ts
+++ src/db/apiAuth.ts
@@ -1,6 +1,7 @@
 import { supabase } from "./supabase";
 
 
+
 export async function login({email, password}: {email: string, password: string}){
 
     const {data, error} = await supabase.auth.signInWithPassword({email, password,});
