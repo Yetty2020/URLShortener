38f2199830e3da6667067fcf572e5a6df4d89d65
Yetty2020 <fatihaadetoro@gmail.com>
2026-04-17 22:28:40 +0100

    feat: Impemented error message for user login and signup

---
+++ src/components/Error.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = ({message}: {message: string}) => {
+  return (
+    <span className='text-sm text-red-400'>{message}</span>
+  );
+}
+
+export default Error;
